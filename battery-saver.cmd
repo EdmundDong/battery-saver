ff9b47c569492a8eb30b8170c70b0d53fb597233
Edmund Dong <12579441+EdmundDong@users.noreply.github.com>
2022-04-26 15:50:30 -0700

    update with watchdog

--- battery-saver.cmd
+++ battery-saver.cmd
@@ -1,3 +1,11 @@
 @echo off
+title Battery Saver
+cls
+:loop
+echo %date:~12,2%-%date:~4,2%-%date:~7,2% %time:~0,8% ^| Watchdog Started
 call conda activate battery-saver
 python battery_saver.py
+echo ***************************************************************
+echo %date:~12,2%-%date:~4,2%-%date:~7,2% %time:~0,8% ^| WARNING: Program closed or crashed, restarting.
+echo ***************************************************************
+goto loop
